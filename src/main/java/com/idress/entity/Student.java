7484935d216a8a2f285bb0f892f4756a5504792b
youxiu326 <youxiu326@163.com>
2019-05-03 09:34:22 +0800

    提交到本地仓库

---
+++ src/main/java/com/idress/entity/Student.java
@@ -0,0 +1,37 @@
+package com.idress.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class Student implements Serializable {
+
+    private String id;
+
+    private String name;
+
+    private Date birthday;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+}
